7d47abbe4f2a1d9b099445e4165b15b0daffc442
robertsja <jarrod.j.roberts@gmail.com>
2024-01-26 12:20:38 +1300

    MVP of web-scraper - can still be optimized and cleaned up but does its job

--- ScrapingDog.py
+++ ScrapingDog.py
@@ -33,7 +33,7 @@ def split_job_count(s):
     :rtype: int
     """
     number = [int(word) for word in s.split() if word.isdigit()][0]
-    print(number)
+    #print(number)
     return number
 
 
@@ -45,16 +45,16 @@ def find_load_more_button(driver):
     :return: Show More Jobs button element
     :rtype: WebElement
     """
-    print("Finding Button")
+    #print("Finding Button")
     time.sleep(.5)
 
     # Button changes locations sometimes, hence the try/catch
     try:
-        print('Clicked first button')
-        return WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/div/button")))
+        #print('Clicked first button')
+        return WebDriverWait(driver, .5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/div/button")))
     except selenium.common.exceptions.TimeoutException:
         try:
-            return WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[1]/div[3]/div[2]/div[1]/div[2]/div/button")))
+            return WebDriverWait(driver, .5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[1]/div[3]/div[2]/div[1]/div[2]/div/button")))
         except selenium.common.exceptions.TimeoutException:
             # If the button is in neither of the expected places and has moved again, this error message is thrown
             try:
@@ -71,7 +71,7 @@ def click_load_more(driver):
     """
     time.sleep(1)
     load_more_button = find_load_more_button(driver)
-    print("Found Button")
+    #print("Found Button")
     load_more_button.click()
 
 
@@ -82,8 +82,12 @@ def close_pop_up(driver):
     :param Webdriver driver: Selenium webdriver
     """
     time.sleep(.5)
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, "CloseButton"))).click()
-    print("Closed Popup")
+    WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CLASS_NAME, "CloseButton"))).click()
+    try:
+        WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CLASS_NAME, "CloseButton"))).click()
+    except:
+        pass
+    #print("Closed Popup")
     return
 
 
@@ -93,28 +97,53 @@ def load_all_jobs(driver):
 
     :param Webdriver driver: Selenium webdriver
     """
-    for i in range(0, get_job_count(driver)//RESULT_PER_PAGE):
+    for i in range(0, job_count//RESULT_PER_PAGE):
         try:
             click_load_more(driver)
         except selenium.common.exceptions.ElementClickInterceptedException: #Catches Exception where popup blocks button
-            print("Found Pop-up")
+            #print("Found Pop-up")
             close_pop_up(driver)
             click_load_more(driver)
     time.sleep(5)
 
 
+def click_job_listing(driver, i):
+    """
+    Clicks a job listing
+
+    :param Webdriver driver: Selenium webdriver
+    :param int i: for loop index, counts which job listing should be getting clicked
+    """
+    xpath1 = "/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/ul/li[" + str(i + 1) + "]/div/div/div[1]"
+    xpath2 = "/html/body/div[3]/div[1]/div[3]/div[2]/div[1]/div[2]/ul/li[" + str(i + 1) + "]/div/div/div[1]"
+    try:
+        try:
+            WebDriverWait(driver, .5).until(EC.element_to_be_clickable((By.XPATH, xpath1))).click()
+        except selenium.common.exceptions.TimeoutException:
+            try:
+                WebDriverWait(driver, .5).until(EC.element_to_be_clickable((By.XPATH, xpath2))).click()
+            except selenium.common.exceptions.TimeoutException:
+                print("Job listing is not correct XPATH")
+    except selenium.common.exceptions.ElementClickInterceptedException:  # Catches Exception where popup blocks button
+        #print("Found Pop-up")
+        close_pop_up(driver)
+        time.sleep(1)
+        click_job_listing(driver, i)
+
+
 # Set up the webdriver
 driver = webdriver.Chrome()
 driver.get(target_url)
-print("Maximizing Window")
+#print("Maximizing Window")
 driver.maximize_window()
-print("Maximized Window")
+#print("Maximized Window")
+
+job_count = get_job_count(driver)
 
 load_all_jobs(driver)
 
 # Retrieve the page source after loading all content
 resp = driver.page_source
-driver.quit()
 
 # Parse the page source with BeautifulSoup
 soup = BeautifulSoup(resp, 'html.parser')
@@ -126,25 +155,46 @@ allJobs = allJobsContainer.find_all("li")
 l = []
 o = {}
 
-for job in allJobs:
+for i in range(job_count):
+    click_job_listing(driver, i)
+    specific_resp = driver.page_source
+    specific_soup = BeautifulSoup(specific_resp, 'html.parser')
+    job_listing_container = specific_soup.find("div", {"class": "JobDetails_jobDetailsContainer__sS1W1"})
     try:
-        o["name-of-company"] = job.find("span", {"class": "EmployerProfile_employerName__Xemli"}).text
+        o["company_name"] = allJobs[i].find("span", {"class": "EmployerProfile_employerName__Xemli"}).text
     except:
-        o["name-of-company"] = None
+        o["company_name"] = None
     try:
-        o["name-of-job"] = job.find("a", {"class": "JobCard_seoLink__WdqHZ"}).text
+        o["job_title"] = allJobs[i].find("a", {"class": "JobCard_seoLink__WdqHZ"}).text
     except:
-        o["name-of-job"] = None
+        o["job_title"] = None
     try:
-        o["location"] = job.find("div", {"class": "JobCard_location__N_iYE"}).text
+        o["location"] = allJobs[i].find("div", {"class": "JobCard_location__N_iYE"}).text
     except:
         o["location"] = None
     try:
-        o["salary"] = job.find("div", {"class": "JobCard_salaryEstimate___m9kY"}).text
+        o["date_listed"] = allJobs[i].find("div", {"class": "JobCard_listingAge__KuaxZ"}).text
+    except:
+        o["date_listed"] = None
+    try:
+        o["long_description"] = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            "//*[@id=\"app-navigation\"]/div[3]/div[2]/div[2]/div[1]/section/div/div[1]"))).get_attribute(
+            "innerText")
+    except:
+        o["long_description"] = None
+    try:
+        o["company_overview"] = job_listing_container.find("div", {"class": "JobDetails_companyOverviewGrid__CV62w"}).text
+    except:
+        o["company_overview"] = None
+    try:
+        o["salary"] = allJobs[i].find("div", {"class": "JobCard_salaryEstimate___m9kY"}).text
     except:
         o["salary"] = None
     l.append(o)
     o = {}
+
+driver.quit()
 
 # Create a DataFrame and save it to a CSV file
 df = pd.DataFrame(l)
